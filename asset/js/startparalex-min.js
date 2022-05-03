b30428fb61e5fc78c032500401946d0d942873a8
Chrystian Lozano <chrystianlozano@icloud.com>
2022-05-03 07:07:57 -0500

    listo

---
+++ asset/js/startparalex-min.js
@@ -0,0 +1,19 @@
+//  var image2 = document.querySelectorAll('.paral2');
+//      new simpleParallax(image2, {
+//          delay: 0.9,
+//          orientation: 'down',
+//          scale: 1.4,
+//          overfow: false,
+//          transition: 'cubic-bezier(0,0,0,1)'
+//      });        
+    //  var image2 = document.getElementsByClassName('paral2');
+    //  new simpleParallax(image2, {
+    //          delay: .6,
+    //         transition: 'cubic-bezier(0,0,0,1)'
+    // });
+
+
+    var image2 = document.getElementsByClassName('paral2');
+    new simpleParallax(image2, {
+        overflow: true
+    });
